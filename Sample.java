8266f43854668d355b782441aea8c330a159a000
Prince <prince@gmail.com>
2024-03-12 11:51:54 +0530

    Updated by prince

--- Sample.java
+++ Sample.java
@@ -6,8 +6,10 @@ class Sample {
     int second = 10;
     
     int sum = first + second;
-    
+    int mul = first * second;
+
     
     System.out.println(sum);
+    System.out,println(mul);
   }
 }
