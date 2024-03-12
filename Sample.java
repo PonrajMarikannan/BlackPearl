ffd5c4e40324496cd44107a1d41cb321726665e8
PONRAJ M <123803809+PonrajMarikannan@users.noreply.github.com>
2024-03-12 11:44:07 +0530

    Create Sample.java

---
+++ Sample.java
@@ -0,0 +1,13 @@
+class Sample {
+
+  public static void main(String[] args) {
+    
+    int first = 20;
+    int second = 10;
+    
+    int sum = first + second;
+    
+    
+    System.out.println(sum);
+  }
+}
